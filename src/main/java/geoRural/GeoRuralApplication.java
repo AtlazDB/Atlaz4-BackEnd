2273c20a682c0f1859a4887d6a7289cbc2ee1615
Gabriel Nunes <gabriel19nunes@gmail.com>
2026-09-14 20:10:18 -0300

    feat(TASK-001): Configurar Docker Compose com Oracle XE e rede entre o container e o Spring Boot

---
+++ src/main/java/geoRural/GeoRuralApplication.java
@@ -0,0 +1,13 @@
+package geoRural;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeoRuralApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeoRuralApplication.class, args);
+	}
+
+}
---
+++ src/test/java/geoRural/GeoRuralApplicationTests.java
@@ -0,0 +1,13 @@
+package geoRural;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeoRuralApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
